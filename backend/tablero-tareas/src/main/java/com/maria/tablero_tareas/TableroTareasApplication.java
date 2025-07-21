c3efe6f4f37894c8077a07aeb6cf58643a0e130d
Angela Gomez <espitiaangela740@gmail.com>
2025-07-21 13:05:26 -0500

    Sprint 0: estructura base y setup inicial

---
+++ backend/tablero-tareas/src/main/java/com/maria/tablero_tareas/TableroTareasApplication.java
@@ -0,0 +1,13 @@
+package com.maria.tablero_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableroTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableroTareasApplication.class, args);
+	}
+
+}
